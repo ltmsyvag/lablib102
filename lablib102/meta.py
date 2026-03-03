a07e3dad8937e0c9340cf3817f96b3cbb03072f8
haiteng <haiteng.wu@outlook.com>
2026-03-03 13:52:45 +0800

    better centroid control

--- lablib102/meta.py
+++ lablib102/meta.py
@@ -30,7 +30,7 @@ class ArrayFrame:
         self.bg_pixel_mean = None
         self.total_pixel_mean = None
         self.std_of_rect_means = None
-        self.centroid_site_id = None # id_X, id_Y
+        self.centroid_of_sites = None # id_X, id_Y
 
     def define_rects(self, x1, y1, x2, y2, x3, y3, nsites_x, nsites_y, rect_side, figsize = (6.4, 4.8), vmax = None, save_path = None):
         vecx = np.array([x2 - x1, y2 - y1])/(nsites_x-1)
@@ -69,8 +69,6 @@ class ArrayFrame:
         self.total_pixel_mean = self.imgarr.mean()
         pixel_means_by_rects = (arr_sums/(rect_side**2)).flatten()
         self.std_of_rect_means = pixel_means_by_rects.std()
-        y0, x0 = ndimage.center_of_mass(arr_sums)
-        self.centroid_site_id = x0, y0
         ## dataframe
         lst_id2d = [(id1d//self.nsites_x, id1d%self.nsites_x) for id1d in range(self.nsites_x*self.nsites_y)]
         self.df = pd.DataFrame(lst_id2d, columns=['id_y', 'id_x'])
@@ -78,14 +76,32 @@ class ArrayFrame:
         self.df['rect_mean'] = pixel_means_by_rects
         self.df['rect_mean_normed'] = self.df['rect_mean']/self.rects_pixel_mean
         self.df[['frame_coord_x', 'frame_coord_y']] = grid_points_int
-        # derived data
-        self.df['r_from_centroid'] = np.sqrt((self.df['id_x'] - x0)**2 + (self.df['id_y'] - y0)**2)
+        
+        ## centroid
+        x0, y0 = self.get_site_centroid(arr_sums)
+        self._update_centroid_and_r(x0, y0)
 
         self.visualize_rects(figsize=figsize, vmax=vmax, save_path=save_path)
-    def _rects_check(self):
+    def _update_centroid_and_r(self, x0, y0):
+        self.centroid_of_sites = x0, y0
+        self.df['r_from_centroid'] = np.sqrt(
+            (self.df['id_x'] - x0)**2
+            + (self.df['id_y'] - y0)**2)
+    def set_centroid_of_sites(self, x0, y0):
+        self._has_rects()
+        self._update_centroid_and_r(x0, y0)
+    @staticmethod
+    def get_site_centroid(zz: np.ndarray):
+        bbgg = np.percentile(zz,50) # bg substraction, kinda important
+        zz_shifted = zz-bbgg
+        zz_shifted[zz_shifted < 0] = 0
+        y0, x0 = ndimage.center_of_mass(zz_shifted)
+        return x0, y0
+    def _has_rects(self):
         if self.rects123 is None:
             raise ValueError("Please call define_rects first to define rects.")
     def visualize_single_rect(self, ix, iy, vmax = None):
+        self._has_rects()
         id1d = iy*self.nsites_x + ix
         x_low_edge, y_low_edge = self._low_edges[id1d]
         block_slice = (slice(y_low_edge, y_low_edge+self.rect_side),
@@ -101,7 +117,7 @@ class ArrayFrame:
         if save_path is not None:
             fig.savefig(save_path, dpi=600)
     def visualize_rects(self, figsize = (6.4, 4.8), vmax = None, save_path = None):
-        self._rects_check()
+        self._has_rects()
         fig, ax = plt.subplots(figsize = figsize)
         extent = 0, self.imgarr.shape[1],  self.imgarr.shape[0], 0
         ax.imshow(self.imgarr, extent=extent, vmax = vmax)
@@ -112,13 +128,13 @@ class ArrayFrame:
         if save_path is not None:
             fig.savefig(save_path, dpi=600)
     def visualize_site_homogeneity(self):
-        self._rects_check()
+        self._has_rects()
         fig, ax = plt.subplots()
         im = ax.imshow(self.df['rect_mean_normed'].values.reshape(self.nsites_y, self.nsites_x))
-        ax.add_patch(Circle(self.centroid_site_id, radius=2, color='white', fill = False))
+        ax.add_patch(Circle(self.centroid_of_sites, radius=2, color='white', fill = False))
         fig.colorbar(im, ax=ax)
     def rects_hist(self):
-        self._rects_check()
+        self._has_rects()
         fig, ax = plt.subplots()
         ax.hist(self.df['rect_mean']/self.rects_pixel_mean, bins=30, label = 'single ROI pixel mean')
         ax.axvline(self.total_pixel_mean/self.rects_pixel_mean, color='red', linestyle='dashed', label='total Pixel Mean')
